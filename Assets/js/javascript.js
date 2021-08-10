53b33b984e2da242dcdb0c749c66cf144c8c1402
Jeffrey McLeod <jeffreymmcleod@outlook.com>
2021-08-10 17:53:17 -0500

    added css, maked timer work

--- Assets/js/javascript.js
+++ Assets/js/javascript.js
@@ -1,4 +1,27 @@
-// Need to make a timer
+var timerEl = document.getElementById('countdown');
+
+// Timer countdown Function
+function countdown() {
+    var timeLeft = 90; 
+
+    var timeInterval = setInterval(function () {
+      if (timeLeft > 1) {
+        timerEl.textContent = " "+ timeLeft + ' seconds remaining';
+        timeLeft--;
+      } else if (timeLeft === 1) {
+        timerEl.textContent = " "+ timeLeft + ' second remaining';
+        timeLeft--;
+      } else {
+        timerEl.textContent = ' Out of Time ';
+        clearInterval(timeInterval);
+      }
+    }, 1000);
+  }
+
+
+//Calling countdown function on page to start when page is loaded  
+countdown();
+
 // Need to make a correct/ incorrect score
 // Need to store local high scores
 // incorrect answers need to affect the timer negatively
